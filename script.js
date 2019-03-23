63d7d6ee6676767d3496bc9931e4c248e4d9f2b0
Lagrek <rain.demorser@gmail.com>
2019-03-23 18:15:15 +0300

    0.3.0

---
+++ script.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+  $('.slider').bxSlider({
+    mode: 'horizontal',
+    speed: 600,
+    slideWidth: 300,
+    slideHeight: 200,
+    startSlide: 0,
+    randomStart: false,
+    infiniteLoop: true,
+    pager:false,
+    responsive: true,
+    captions: true,
+    auto: true,
+    pause: 5000,
+  });
+  });
